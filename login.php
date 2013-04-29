4b0b1fec373dccb937a36c9a21470e940d2f9c0c
Khalid <kyour@hotmail.com>
2013-04-29 20:49:48 +0500

    heloo

--- login.php
+++ login.php
@@ -9,8 +9,13 @@
 </form>
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 </form>
+<div class="login-error error">
+</div>
+>>>>>>> b697d0c532dbba5b3327490a60ca2eb67518312a
+=======
 <div class="login-error error">
 </div>
 >>>>>>> b697d0c532dbba5b3327490a60ca2eb67518312a
