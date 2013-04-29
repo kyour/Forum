0fca2d947d995021696b4526cfed147ec352d9e8
Khalid <kyour@hotmail.com>
2013-04-29 20:45:12 +0500

    hello

--- login.php
+++ login.php
@@ -7,8 +7,13 @@
 <input type="submit" name="submit" value="Sign In">
 <<<<<<< HEAD
 </form>
+<<<<<<< HEAD
 =======
 </form>
+<div class="login-error error">
+</div>
+>>>>>>> b697d0c532dbba5b3327490a60ca2eb67518312a
+=======
 <div class="login-error error">
 </div>
 >>>>>>> b697d0c532dbba5b3327490a60ca2eb67518312a
