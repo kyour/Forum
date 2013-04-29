97972c2c786e240ac5b2c312d13510c3ad57f5ed
Akmal <oceankhan_180@yahoo.com>
2013-04-29 19:53:02 +0500

    categories are upadated

---
+++ cat-list.php
@@ -0,0 +1,10 @@
+<?php
+include ("includes/connection.php");
+$query=mysql_query("SELECT * FROM category");
+
+while ($rowshow=mysql_fetch_array($query)){
+	$_id = $rowshow["cat_id"];
+	$_name = $rowshow["cat_name"];
+	echo $_name."<br><span><a href='cat-detail.php?id=".$_id."'>Details</a></span>";
+}
+?>
