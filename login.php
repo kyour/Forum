a5c1bd38430b1fa996309691b9374b2329785d3f
Khalid <kyour@hotmail.com>
2013-04-29 19:12:28 +0500

    commit

--- login.php
+++ login.php
@@ -1,8 +1,14 @@
 <h2>Login</h2>
 <form id="logIn-form" action="login-exe.php" method="post">
 <label>Email</label>
-<input type="email" name="email" id="email"><br />
+<input type="email" name="email"><br />
 <label>Password</label>
-<input type="text" name="password" id="password"><br />
+<input type="text" name="password"><br />
 <input type="submit" name="submit" value="Sign In">
+<<<<<<< HEAD
 </form>
+<div class="login-error error">
+</div>
+=======
+</form>
+>>>>>>> some tweaks in css, js, login and registration files
