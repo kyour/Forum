ed2743548127600659ea6445aede45756e2af808
Khalid <kyour@hotmail.com>
2013-04-29 19:16:13 +0500

    commit

--- login.php
+++ login.php
@@ -9,10 +9,14 @@
 </form>
 <div class="login-error error">
 <<<<<<< HEAD
+<<<<<<< HEAD
 </div>
 =======
 </form>
 >>>>>>> some tweaks in css, js, login and registration files
+=======
+</div>
+>>>>>>> 96ad4896a530f3b03d559a55f3c2ac6c310a579f
 =======
 </div>
 >>>>>>> 96ad4896a530f3b03d559a55f3c2ac6c310a579f
